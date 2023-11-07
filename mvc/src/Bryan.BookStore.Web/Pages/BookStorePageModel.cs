7af9c974cfd999907e255fa24e507490b76ab639
Bryan Jerahmeel Yao <bgyao@live.com>
2023-11-07 09:07:13 +0800

    MVC / Razor Pages setup

---
+++ mvc/src/Bryan.BookStore.Web/Pages/BookStorePageModel.cs
@@ -0,0 +1,14 @@
+﻿using Bryan.BookStore.Localization;
+using Volo.Abp.AspNetCore.Mvc.UI.RazorPages;
+
+namespace Bryan.BookStore.Web.Pages;
+
+/* Inherit your PageModel classes from this class.
+ */
+public abstract class BookStorePageModel : AbpPageModel
+{
+    protected BookStorePageModel()
+    {
+        LocalizationResourceType = typeof(BookStoreResource);
+    }
+}
