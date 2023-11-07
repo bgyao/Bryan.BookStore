0cbcdf64c2da587a58170940a973e3be2549f453
Bryan Jerahmeel Yao <bgyao@live.com>
2023-11-07 13:54:03 +0800

    Books Entity - backend

---
+++ mvc/src/Bryan.BookStore.Domain.Shared/Books/BookType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bryan.BookStore.Books;
+
+public enum BookType
+{
+    Undefined,
+    Adventure,
+    Biography,
+    Dystopia,
+    Fantasy,
+    Horror,
+    Science,
+    ScienceFiction,
+    Poetry
+}
