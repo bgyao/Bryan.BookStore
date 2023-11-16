1dda0b3646b9deece482d22cf967921d79cdc478
Bryan Jerahmeel Yao <bgyao@live.com>
2023-11-16 19:59:50 +0800

    Books Backend

---
+++ mvc/src/Bryan.BookStore.Application.Contracts/Books/Dtos/CreateUpdateBookDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Bryan.BookStore.Books.Dtos;
+
+public class CreateUpdateBookDto
+{
+    [Required]
+    [StringLength(28)]
+    public string Name { get; set; }
+
+    [Required]
+    public BookType Type { get; set; }
+
+    [Required]
+    [DataType(DataType.Date)]
+    public DateTime PublishDate { get; set; }
+
+    [Required]
+    public float Price { get; set; }
+}
---
+++ mvc/src/Bryan.BookStore.Application/Books/BookAutoMapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Bryan.BookStore.Books.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bryan.BookStore.Books;
+
+public class BookAutoMapperProfile : Profile
+{
+    public BookAutoMapperProfile()
+    {
+        CreateMap<Book, BookDto>();
+        CreateMap<CreateUpdateBookDto, Book>();
+    }
+}
